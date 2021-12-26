e072be8f1fe8c51ac2348c8b6663a56afc1091ae
Harley McKenzie <harley@akoova.com>
2021-12-26 12:26:13 +0000

    Removing pre-configured variable values. This should now be configured using either tfvars or variables in TF Cloud

--- variables.tf
+++ variables.tf
@@ -1,34 +1,28 @@
 variable "web-ami" {
   type    = string
-  default = "ami-04c2a5c7e6c051fb2"
 }
 
 variable "web-instance-type" {
   type    = string
-  default = "t4g.micro"
+  default = "t2.micro"
 }
 
 variable "keypair" {
     type = string
-    default = "hmckenzie-london"
 }
 
 variable "hostname" {
   type    = string
-  default = "hmckenzie.net"
 }
 
 variable "codepipeline-bucket" {
   type = string
-  default = "hmckenzie-codepipeline"
 }
 
 variable "web-bucket" {
   type = string
-  default = "hmckenzie-web-content"
 }
 
 variable "codestar-connection-arn" {
   type = string
-  default = "arn:aws:codestar-connections:eu-west-1:077965743439:connection/8eabd531-72a2-44b3-bd85-03614028ae60"
 }
