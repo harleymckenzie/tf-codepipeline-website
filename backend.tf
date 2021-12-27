f4aaa2df593377bcd156d8288e5688a2087fa36f
Harley McKenzie <mckenzie.harley@gmail.com>
2021-12-27 13:53:56 +0000

    Updated terraform backend config to use new tagging feature in 1.1.0

--- backend.tf
+++ backend.tf
@@ -1,7 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket = "hmckenzie-tf"
-    key    = "stack/hmckenzie-web/tfstate"
-    region = "eu-west-2"
+  cloud {
+    organization = "hmckenzie"
+    workspaces {
+      tags = [
+        "hmckenzie:web"
+      ]
+    }
   }
 }
