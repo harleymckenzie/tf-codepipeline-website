e399df648c51eed41f713d36207fbbb4124206df
Harley McKenzie <mckenzie.harley@gmail.com>
2021-12-30 12:35:51 +0000

    Added module documentation

--- modules/apache-web-app/main.tf
+++ modules/apache-web-app/main.tf
@@ -1,3 +1,22 @@
+########################################################
+# Module Requirements
+########################################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+  }
+
+  required_version = "=> 0.13.0"
+}
+
+########################################################
+# User Data Data Source
+########################################################
+
 data "template_file" "userdata" {
   template = "${file("${path.module}/userdata.tpl")}"
   vars = {
